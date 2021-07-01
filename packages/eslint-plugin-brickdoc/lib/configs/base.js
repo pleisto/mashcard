714e5e4da08d048e6a82c285b1eed77ab27e2d23
Saiqi Jia <jsq2627@gmail.com>
2021-07-01 14:21:54 +0800

    refactor: cleanup & greenkeeping (#264)
    1. Upgrade all front end dependencies
    2. Separate eslint and prettier into two stages during lint-staged, and enable prettier for other supported file formats.
    3. Use caret version ranges (`^x.y.z`) for all dependencies, which should be more friendly with greenkeeping bots (e.g. dependabot).
    4. Fix incorrectly placed dependencies for `@brickdoc/eslint-plugin`. [Generally](https://eslint.org/docs/developer-guide/shareable-configs#publishing-a-shareable-config) these eslint plugins should be placed into `peerDependencies`

--- packages/eslint-plugin-brickdoc/lib/configs/base.js
+++ packages/eslint-plugin-brickdoc/lib/configs/base.js
@@ -1,12 +1,8 @@
 module.exports = {
-  extends: [
-    'standard-with-typescript',
-    'plugin:jest/recommended',
-    'prettier'
-  ],
-  plugins: ['import', 'jest', 'prettier'],
+  extends: ['standard-with-typescript', 'plugin:jest/recommended', 'prettier'],
+  plugins: ['import', 'jest'],
   parserOptions: {
-    project: ['./tsconfig.json'], // Specify it only for TypeScript files
+    project: ['./tsconfig.json'] // Specify it only for TypeScript files
   },
   settings: {
     // Some config suggestions copied from https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration
@@ -18,36 +14,30 @@ module.exports = {
         alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory
         project: process.cwd()
       }
-    },
+    }
   },
   env: {
     'jest/globals': true,
     browser: true
   },
-  ignorePatterns: [
-    'coverage',
-    'dist',
-    'temp',
-    'node_modules',
-    "**/__snapshots__"
-  ],
+  ignorePatterns: ['coverage', 'dist', 'temp', 'node_modules', '**/__snapshots__'],
   rules: {
     semi: ['error', 'never'],
     'for-direction': 'error',
-    'func-name-matching': ["error", "always"],
+    'func-name-matching': ['error', 'always'],
     'getter-return': 'error',
     'grouped-accessor-pairs': 'error',
-    'max-depth': ['error',5],
-    'max-nested-callbacks': ['error',3],
-    'max-params': ['error',4],
+    'max-depth': ['error', 5],
+    'max-nested-callbacks': ['error', 3],
+    'max-params': ['error', 4],
     'no-inner-declarations': ['error', 'both'],
     'no-dupe-else-if': 'error',
     'no-duplicate-imports': 'error',
     'no-implicit-coercion': [
       'error',
       {
-        allow: ['!!'],
-      },
+        allow: ['!!']
+      }
     ],
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/restrict-plus-operands': 'off',
@@ -56,48 +46,51 @@ module.exports = {
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/unified-signatures': 'error',
-    '@typescript-eslint/prefer-ts-expect-error':'off',
+    '@typescript-eslint/prefer-ts-expect-error': 'off',
     '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/member-ordering': ["error", {
-      "default": [
-        "public-static-field",
-        "protected-static-field",
-        "private-static-field",
-        "static-field",
-        "public-static-method",
-        "protected-static-method",
-        "private-static-method",
-        "static-method",
-        "public-instance-field",
-        "protected-instance-field",
-        "private-instance-field",
-        "public-field",
-        "protected-field",
-        "private-field",
-        "instance-field",
-        "field",
-        "constructor",
-        "public-instance-method",
-        "protected-instance-method",
-        "private-instance-method",
-        "public-method",
-        "protected-method",
-        "private-method",
-        "instance-method",
-        "method"
-      ]
-    }],
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          'public-static-field',
+          'protected-static-field',
+          'private-static-field',
+          'static-field',
+          'public-static-method',
+          'protected-static-method',
+          'private-static-method',
+          'static-method',
+          'public-instance-field',
+          'protected-instance-field',
+          'private-instance-field',
+          'public-field',
+          'protected-field',
+          'private-field',
+          'instance-field',
+          'field',
+          'constructor',
+          'public-instance-method',
+          'protected-instance-method',
+          'private-instance-method',
+          'public-method',
+          'protected-method',
+          'private-method',
+          'instance-method',
+          'method'
+        ]
+      }
+    ],
     'no-alert': 'error',
     'no-bitwise': 'error',
     'no-var': 'error',
     'no-continue': 'error',
     'no-lonely-if': 'error',
     'no-loop-func': 'error',
-    'complexity': [
+    complexity: [
       'error',
       {
-        max: 20,
-      },
+        max: 20
+      }
     ],
     'max-classes-per-file': ['error', 1],
     'no-nested-ternary': 'error',
@@ -115,20 +108,23 @@ module.exports = {
         skipStrings: true,
         skipComments: false,
         skipRegExps: true,
-        skipTemplates: true,
-      },
+        skipTemplates: true
+      }
     ],
     'object-shorthand': 'error',
     'prefer-template': 'error',
     'prefer-destructuring': ['error', { array: false }],
-    'max-len': ['error', {
-      code: 140,
-      tabWidth: 2,
-      ignoreTemplateLiterals: true,
-      ignoreUrls: true,
-      ignoreTrailingComments: true,
-      ignoreStrings: true
-    } ],
+    'max-len': [
+      'error',
+      {
+        code: 140,
+        tabWidth: 2,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+        ignoreTrailingComments: true,
+        ignoreStrings: true
+      }
+    ],
     'no-restricted-globals': [
       'error',
       ...['blur', 'close', 'focus', 'length', 'name', 'parent', 'self', 'stop'].map(name => ({
