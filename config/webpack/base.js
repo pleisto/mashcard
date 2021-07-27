f6efba2460b63cf2efb4a2dad929616e90377b8e
Saiqi Jia <jsq2627@gmail.com>
2021-07-28 00:32:12 +0800

    build(deps): upgrade dependencies (#365)
    * build(deps): upgrade dependencies
    
    * fix: fix build errors & improve auto upgrade script
    
    Co-authored-by: 0xDing <0xDing@users.noreply.github.com>

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const { ProgressPlugin } = require('webpack')
 const { webpackConfig, merge } = require('@rails/webpacker')
-const WebpackAssetsManifest = require('webpack-assets-manifest')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const DistAssetResolvePlugin = require('./DistAssetResolvePlugin')
 
@@ -19,23 +18,9 @@ webpackConfig.module.rules
   .find(x => x.test.toString().includes('jsx'))
   .use.push({ loader: 'ts-loader', options: { transpileOnly: true, projectReferences: true, configFile: 'tsconfig.build.json' } })
 
-// Reconfigure WebpackAssetsManifest to Fix https://github.com/rails/webpacker/issues/2864
-webpackConfig.plugins = webpackConfig.plugins.filter(x => x.constructor.name !== 'WebpackAssetsManifest')
-
 module.exports = merge(webpackConfig, {
   resolve: {
     plugins: [new TsconfigPathsPlugin({ configFile: path.resolve(__dirname, '../../tsconfig.build.json') }), new DistAssetResolvePlugin()]
   },
-  plugins: [
-    new ProgressPlugin(),
-    new WebpackAssetsManifest({
-      enabled: true,
-      entrypoints: true,
-      writeToDisk: true,
-      output: 'manifest.json',
-      entrypointsUseAssets: true,
-      space: 2,
-      publicPath: true
-    })
-  ]
+  plugins: [new ProgressPlugin()]
 })
