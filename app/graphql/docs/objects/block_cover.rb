daba4a51eb1637d20c1aaee8c04bafb171c50d2a
shiyuhang <shiyuhang@brickdoc.com>
2021-08-03 10:51:10 +0800

    fix: add resolve_type

--- app/graphql/docs/objects/block_cover.rb
+++ app/graphql/docs/objects/block_cover.rb
@@ -3,6 +3,11 @@ module Docs
   module Objects
     class BlockCover < BrickGraphQL::BaseUnion
       possible_types BlockImage, BlockColor
+
+      def self.resolve_type(object, _ctx)
+        type_name = "block_#{object.type}".classify
+        "Docs::Objects::#{type_name}".safe_constantize
+      end
     end
   end
 end
--- app/graphql/docs/objects/block_icon.rb
+++ app/graphql/docs/objects/block_icon.rb
@@ -3,6 +3,11 @@ module Docs
   module Objects
     class BlockIcon < BrickGraphQL::BaseUnion
       possible_types BlockImage, BlockEmoji
+
+      def self.resolve_type(object, _ctx)
+        type_name = "block_#{object.type}".classify
+        "Docs::Objects::#{type_name}".safe_constantize
+      end
     end
   end
 end
