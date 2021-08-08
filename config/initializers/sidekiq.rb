838515eebe2428d06131f6320eb2daf728d0d6ba
Li (Boris) Ding <ding@brickdoc.com>
2021-08-08 22:40:59 +0800

    style: fixed rubocop

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
- Sidekiq.configure_server do |config|
-    config.redis = Brickdoc::Redis.pool(:queue)
-  end
+# frozen_string_literal: true
+Sidekiq.configure_server do |config|
+  config.redis = Brickdoc::Redis.pool(:queue)
+end
 
-  Sidekiq.configure_client do |config|
-    config.redis = Brickdoc::Redis.pool(:queue)
-  end
+Sidekiq.configure_client do |config|
+  config.redis = Brickdoc::Redis.pool(:queue)
+end
