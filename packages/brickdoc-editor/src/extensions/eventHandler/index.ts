281ab38ac8c33a9c69bdc9e1306084a7b7345839
darmody <darmody@brickdoc.com>
2022-01-25 15:43:07 +0800

    feat(@brickdoc/editor): allow delete blocks through Backspace

--- packages/brickdoc-editor/src/extensions/eventHandler/index.ts
+++ packages/brickdoc-editor/src/extensions/eventHandler/index.ts
@@ -1,7 +1,7 @@
 import { Extension } from '@tiptap/core'
 import { Plugin, PluginKey } from 'prosemirror-state'
 import { pasteImageHandler } from './pasteImageHandler'
-import { backspaceHandler } from './backspaceHandler'
+// import { backspaceHandler } from './backspaceHandler'
 import { gapClickHandler } from './gapClickHandler'
 
 export const EventHandlerExtension = Extension.create({
@@ -16,13 +16,13 @@ export const EventHandlerExtension = Extension.create({
           handlePaste(view, event, slice): boolean {
             return pasteImageHandler(editor, event)
           },
-          handleKeyDown(view, event): boolean {
-            if (event.code === 'Backspace') {
-              return backspaceHandler(editor, view, event)
-            }
+          // handleKeyDown(view, event): boolean {
+          //   if (event.code === 'Backspace') {
+          //     return backspaceHandler(editor, view, event)
+          //   }
 
-            return false
-          },
+          //   return false
+          // },
           handleClick(view, pos, event) {
             gapClickHandler(editor, view, pos, event)
             return false
