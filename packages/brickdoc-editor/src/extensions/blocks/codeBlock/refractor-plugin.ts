fa1711dd7efc5976804255c493316d434a22e17e
darmody <darmody@brickdoc.com>
2022-06-17 19:22:28 +0800

    style(@brickdoc/editor): fix read-only type error

--- packages/brickdoc-editor/src/extensions/blocks/codeBlock/refractor-plugin.ts
+++ packages/brickdoc-editor/src/extensions/blocks/codeBlock/refractor-plugin.ts
@@ -142,7 +142,8 @@ export function RefractorPlugin({
 
     props: {
       decorations(state) {
-        return this.getState(state)
+        // this is a tiptap type issue
+        return (this as Plugin).getState(state)
       }
     }
   })
