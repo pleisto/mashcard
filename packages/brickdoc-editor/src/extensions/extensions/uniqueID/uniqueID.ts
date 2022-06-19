2e4dee56feccea0bfe52365b7bc5932b1cfee49d
aligo Kang <aligox@gmail.com>
2022-06-19 21:26:57 +0800

    feat(collaboration): fix unique-id bug

--- packages/brickdoc-editor/src/extensions/extensions/uniqueID/uniqueID.ts
+++ packages/brickdoc-editor/src/extensions/extensions/uniqueID/uniqueID.ts
@@ -1,4 +1,3 @@
-import { findChildren } from '@tiptap/core'
 import { UniqueID as TiptapUniqueID } from '@tiptap/extension-unique-id'
 import { meta } from './meta'
 
@@ -6,22 +5,7 @@ export const UniqueID = TiptapUniqueID.extend({
   name: meta.name,
 
   onCreate() {
-    const { state } = this.editor
-    const { tr, doc } = state
-    const { types, attributeName, generateID } = this.options
-    const nodesWithoutId = findChildren(doc, node => {
-      return types.includes(node.type.name) && node.attrs[attributeName] === null
-    })
-
-    nodesWithoutId.forEach(({ node, pos }) => {
-      tr.setNodeMarkup(pos, undefined, {
-        ...node.attrs,
-        [attributeName]: generateID()
-      })
-    })
-
     // prevent empty document commiting when editor created
-    // view.dispatch(tr)
   }
 })
 
