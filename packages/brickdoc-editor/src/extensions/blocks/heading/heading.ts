89c108b2711809d06c1de847be1a6970b1922859
darmody <darmody@brickdoc.com>
2022-06-20 16:48:02 +0800

    fix(editor): aligns block actions menu to heading text
    fix #32

--- packages/brickdoc-editor/src/extensions/blocks/heading/heading.ts
+++ packages/brickdoc-editor/src/extensions/blocks/heading/heading.ts
@@ -5,6 +5,7 @@ import { meta } from './meta'
 
 export const Heading = TiptapHeading.extend({
   name: meta.name,
+  // specify marks for excluding anchor mark
   marks: 'bold italic link strike textStyle discussion',
   draggable: true,
   addNodeView() {
