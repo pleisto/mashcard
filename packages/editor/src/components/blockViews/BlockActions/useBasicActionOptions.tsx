20976127f5becbc1c1dbbdfb954c5514c3ef870b
kail <kmno4k2mno4@gmail.com>
2022-06-21 15:31:39 +0800

    fix(editor): fix turn into menu style

--- packages/editor/src/components/blockViews/BlockActions/useBasicActionOptions.tsx
+++ packages/editor/src/components/blockViews/BlockActions/useBasicActionOptions.tsx
@@ -3,32 +3,17 @@ import { __serializeForClipboard } from 'prosemirror-view'
 import { NodeSelection } from 'prosemirror-state'
 import { Cutting, Link, Delete, CornerDownRight } from '@mashcard/design-icons'
 import { BlockCommandItem } from '../../../helpers/block'
-import * as BLOCK from '../../../helpers/block'
+import { TRANS_TYPE_LIST } from '../../../helpers/blockConstant'
 import { useDocumentEditable, useEditorContext, useEditorI18n } from '../../../hooks'
 import { ActionGroupOption, ActionItemOption } from './BlockActions'
 import { useBlockContext } from '../../../hooks/useBlockContext'
+import { NodeIcon } from '../../../components/extensionViews/BubbleMenu/useBubbleMenuItems/useBubbleMenuItems'
 
 export type BasicActionOptionType = 'delete' | 'cut' | 'copy' | 'transform'
 
 export interface UseActionOptionsProps {
   types: BasicActionOptionType[]
 }
-
-const transformBlocks = [
-  BLOCK.PARAGRAPH,
-  BLOCK.HEADING_1,
-  BLOCK.HEADING_2,
-  BLOCK.HEADING_3,
-  BLOCK.HEADING_4,
-  BLOCK.HEADING_5,
-  BLOCK.ORDERED_LIST,
-  BLOCK.BULLETED_LIST,
-  BLOCK.TASK_LIST,
-  BLOCK.FORMULA,
-  BLOCK.CODE,
-  BLOCK.BLOCKQUOTE,
-  BLOCK.CALLOUT
-]
 
 export function useBasicActionOptions({ types }: UseActionOptionsProps): ActionGroupOption | null {
   const { deleteBlock, getPosition, contentForCopy, node } = useBlockContext()
@@ -52,7 +37,7 @@ export function useBasicActionOptions({ types }: UseActionOptionsProps): ActionG
       type: 'item',
       name: blockItem.key,
       label: t(`blocks.${blockItem.key}.label`),
-      icon: blockItem.squareIcon,
+      icon: <NodeIcon>{blockItem.icon}</NodeIcon>,
       onAction: () => {
         if (!setNodeSelection()) return
         blockItem.setBlock(editor!.chain()).run()
@@ -125,7 +110,12 @@ export function useBasicActionOptions({ types }: UseActionOptionsProps): ActionG
         name: 'transform',
         type: 'subMenu',
         icon: <CornerDownRight />,
-        items: transformBlocks.filter(i => !!i).map(item => createActionOption(item!))
+        items: TRANS_TYPE_LIST.map(types => {
+          return {
+            type: 'group',
+            items: types.items.map(item => createActionOption(item!))
+          }
+        })
       })
     }
 
--- packages/editor/src/components/extensionViews/BubbleMenu/useBubbleMenuItems/useNodeGroup.tsx
+++ packages/editor/src/components/extensionViews/BubbleMenu/useBubbleMenuItems/useNodeGroup.tsx
@@ -2,27 +2,11 @@ import { cloneElement, useMemo } from 'react'
 import { theme } from '@mashcard/design-system'
 import { ToolbarOption, ToolbarGroupOption } from '../../../ui'
 import { findFirstSelectedNodes } from '../../../../helpers/selection'
-import { BlockCommandItem } from '../../../../helpers/block'
-import * as BLOCK from '../../../../helpers/block'
+
+import { TRANS_TYPE_LIST } from '../../../../helpers/blockConstant'
 import { isBubbleMenuVisible } from '../BubbleMenu'
 import { NodeIcon } from './useBubbleMenuItems'
 import { useEditorContext, useEditorI18n } from '../../../../hooks'
-
-const blockItems: BlockCommandItem[] = [
-  BLOCK.PARAGRAPH,
-  BLOCK.HEADING_1,
-  BLOCK.HEADING_2,
-  BLOCK.HEADING_3,
-  BLOCK.HEADING_4,
-  BLOCK.HEADING_5,
-  BLOCK.ORDERED_LIST,
-  BLOCK.BULLETED_LIST,
-  BLOCK.TASK_LIST,
-  BLOCK.FORMULA,
-  BLOCK.CODE,
-  BLOCK.BLOCKQUOTE,
-  BLOCK.CALLOUT
-]
 
 export function useNodeGroup(): [ToolbarOption | ToolbarGroupOption | null] {
   const { editor } = useEditorContext()
@@ -42,23 +26,22 @@ export function useNodeGroup(): [ToolbarOption | ToolbarGroupOption | null] {
           label: t(`bubble_menu.node.items.${nodeKey ?? 'paragraph'}`),
           tooltip: t('bubble_menu.node.section_title') as string,
           trigger: 'hover',
-          items: [
-            {
+          items: TRANS_TYPE_LIST.map(blocks => {
+            return {
               type: 'group',
-              title: t('bubble_menu.node.section_title'),
-              items: blockItems.map(item => ({
+              items: blocks.items.map(item => ({
                 type: 'item',
                 name: item.key,
                 active: nodeKey === item.key,
                 label: t(`blocks.${item.key}.label`),
-                icon: <NodeIcon>{cloneElement(item.icon, { fill: [theme.colors.iconPrimary.value] })}</NodeIcon>,
+                icon: <NodeIcon>{item.icon}</NodeIcon>,
                 onAction: () => {
                   if (!editor) return
                   item.setBlock(editor.chain()).setTextSelection(editor.state.selection.to).focus().run()
                 }
               }))
             }
-          ]
+          })
         }
       ]
     }
