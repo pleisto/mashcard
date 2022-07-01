1b52c452149a74122866132cea3096caf599c2c4
aligo <aligox@gmail.com>
2022-07-01 17:11:56 +0800

    fix(colors-warning): use Hex string directly for cursor color (#184)
    * fix(colors-warning): use Hex string directly for cursor color

--- apps/client-web/src/docs/utils/__tests__/cursorColor.test.ts
+++ apps/client-web/src/docs/utils/__tests__/cursorColor.test.ts
@@ -2,6 +2,6 @@ import { getCursorColor } from '../cursorColor'
 
 describe('cursorColor', () => {
   it('should can get cursorColor', () => {
-    expect(getCursorColor('test')).toBe('var(--mc-colors-orange8)')
+    expect(getCursorColor('test')).toBe('#c84116')
   })
 })
