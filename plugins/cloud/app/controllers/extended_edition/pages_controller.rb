b6484e9f754f570eacb045b5d180d8f20deff195
Li (Boris) Ding <ding@brickdoc.com>
2022-07-19 17:09:22 +0800

    fix(cloud-plugin): fix landing page redirect

--- plugins/cloud/app/controllers/extended_edition/pages_controller.rb
+++ plugins/cloud/app/controllers/extended_edition/pages_controller.rb
@@ -12,7 +12,7 @@ module ExtendedEdition
 
     # Redirect to landing page if the user is not signed in and visiting the root path.
     def redrect_to_landing_page
-      redirect_to('https://mashcaed.io/') if request.path == '/' && current_user.blank?
+      redirect_to('https://mashcaed.io/', allow_other_host: true) if request.path == '/' && current_user.blank?
     end
   end
 end
