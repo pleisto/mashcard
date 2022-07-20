7fe4da64d8eb5104474ca382bdf8e5ae48c4c7df
kail Wang <kmno4k2mno4@gmail.com>
2022-07-20 13:06:08 +0800

    chore: update snap

--- apps/client-web/src/docs_legacy/utils/__tests__/cursorColor.test.ts
+++ apps/client-web/src/docs_legacy/utils/__tests__/cursorColor.test.ts
@@ -1,7 +1,8 @@
 import { getCursorColor } from '../cursorColor'
+import { theme } from '@mashcard/design-system'
 
 describe('cursorColor', () => {
   it('should can get cursorColor', () => {
-    expect(getCursorColor('test')).toBe('#c84116')
+    expect(getCursorColor('test')).toBe(theme.colors.orange8.value)
   })
 })
