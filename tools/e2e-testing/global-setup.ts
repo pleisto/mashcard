87c1d724396d41224057a10e38fe2121d0c4dcde
Zhekai Ji <jizhekai@gmail.com>
2022-08-01 09:30:33 +0800

    test: update to always get new cookies

--- tools/e2e-testing/global-setup.ts
+++ tools/e2e-testing/global-setup.ts
@@ -1,6 +1,5 @@
 import { BrowserType, chromium, firefox, request, webkit } from '@playwright/test'
 import path from 'path'
-import fs from 'fs'
 
 const browserList: { [key: string]: BrowserType } = {
   chromium,
@@ -16,43 +15,41 @@ async function globalSetup(): Promise<void> {
   const userParams = `name=${project}-${module}&email=${project}-${module}@mashcard.com`
   const fileName = path.join(`./storage/${project}-${module}.json`)
 
-  if (!fs.existsSync(fileName)) {
-    const requestContext = await request.newContext()
-    const response = await requestContext.post(`${baseURL}$internal-apis/accounts/auth/developer/callback`, {
-      data: userParams,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
+  const requestContext = await request.newContext()
+  const response = await requestContext.post(`${baseURL}$internal-apis/accounts/auth/developer/callback`, {
+    data: userParams,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
 
-    await requestContext.storageState({ path: fileName })
-    await requestContext.dispose()
+  await requestContext.storageState({ path: fileName })
+  await requestContext.dispose()
 
-    // When the user not exist, will redirect to sign up page to create it.
-    if (response.url().includes('/accounts/sign-up')) {
-      const browser = await browserList[project].launch()
-      const context = await browser.newContext({ baseURL, storageState: fileName })
-      const page = await context.newPage()
-      try {
-        await context.tracing.start({ screenshots: true, snapshots: true })
+  // When the user not exist, will redirect to sign up page to create it.
+  if (response.url().includes('/accounts/sign-up')) {
+    const browser = await browserList[project].launch()
+    const context = await browser.newContext({ baseURL, storageState: fileName })
+    const page = await context.newPage()
+    try {
+      await context.tracing.start({ screenshots: true, snapshots: true })
 
-        await page.goto('/accounts/sign-up')
-        await page.waitForSelector('main div h1:has-text("Sign Up with Developer")')
-        await page.locator('button[type="submit"]').click()
-        await page.waitForNavigation()
-        await page.context().storageState({ path: fileName })
+      await page.goto('/accounts/sign-up')
+      await page.waitForSelector('main div h1:has-text("Sign Up with Developer")')
+      await page.locator('button[type="submit"]').click()
+      await page.waitForNavigation()
+      await page.context().storageState({ path: fileName })
 
-        await context.tracing.stop({
-          path: './test-results/setup-trace.zip'
-        })
-        await page.close()
-      } catch (error) {
-        await context.tracing.stop({
-          path: './test-results/failed-setup-trace.zip'
-        })
-        await page.close()
-        throw error
-      }
+      await context.tracing.stop({
+        path: './test-results/setup-trace.zip'
+      })
+      await page.close()
+    } catch (error) {
+      await context.tracing.stop({
+        path: './test-results/failed-setup-trace.zip'
+      })
+      await page.close()
+      throw error
     }
   }
 }
